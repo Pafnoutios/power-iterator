04c57bae9ec97e6f1b36d0942f8faadc5808948c
John Szwast <jszwast@yahoo.com>
2014-07-12 15:17:23 -0700

    Starting an interface.

--- include/combination_iterator.h
+++ include/combination_iterator.h
@@ -8,5 +8,16 @@
 
 namespace szwast
 {
+	template<typename K, class ... args>
+	class combinations
+	{
+		typename template<K, args> Container;
+		typename Container::const_iterator;
 
+	public:
+		combinations(Container::const_iterator begin, Iter end, size_t size);
+
+	private:
+
+	};
 }
