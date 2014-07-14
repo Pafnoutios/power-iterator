04cb82bba473b90f5381eaf31e365f3da480b111
John Szwast <jszwast@yahoo.com>
2014-07-13 17:24:29 -0700

    Can construct a "combinations" container.

--- include/combination_iterator.h
+++ include/combination_iterator.h
@@ -6,18 +6,41 @@
  */
 
 
+ #include <memory>
+
+
 namespace szwast
 {
-	template<typename K, class ... args>
+	template<typename Key, class Compare = std::less<Key>, class Allocator = std::allocator<Key>>
 	class combinations
 	{
-		typename template<K, args> Container;
-		typename Container::const_iterator;
-
 	public:
-		combinations(Container::const_iterator begin, Iter end, size_t size);
+		using value_type      = std::set<Key, Compare, Allocator>;
+		using allocator_type  = Allocator;
+		using size_type       = std::size_t;
+
+		using source_iterator = typename value_type::const_iterator;
+
+		class const_iterator;
+
+		combinations(source_iterator source_begin, source_iterator source_end, size_type size);
+
+		const_iterator begin();
+		const_iterator end();
 
 	private:
-
+		source_iterator m_begin;
+		source_iterator m_end;
+		size_type m_size;
 	};
+}
+
+
+template<typename Key, class Compare, class Allocator>
+szwast::combinations<Key, Compare, Allocator>::combinations(source_iterator source_begin, source_iterator source_end, size_type size)
+:	m_begin(source_begin)
+,	m_end(source_end)
+,	m_size(size)
+{
+
 }
