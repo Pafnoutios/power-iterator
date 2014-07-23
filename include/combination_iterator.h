0a74dbfd1efe1728301e8e07d48747738b2af2cc
John Szwast <jszwast@yahoo.com>
2014-07-22 21:46:58 -0700

    Yay!!  Basic combinations iteration works!

--- include/combination_iterator.h
+++ include/combination_iterator.h
@@ -86,12 +86,13 @@ namespace szwast
 			, m_end(source_end)
 			, m_r(r)
 			, m_members(m_r)
+			, m_at_end(end)
 			{
 				source_iterator gopher = m_begin;
 
-				if (end)
+				if (m_at_end)
 				{
-					// Start gopher at m_size before end, to put the last m_member at end.
+					// Start gopher at m_size-1 before end, to put the last m_member at end.
 					gopher = m_end;
 					for (size_type i = 1; (i < m_r) && (gopher != m_begin); i++)
 					{
@@ -107,21 +108,75 @@ namespace szwast
 						gopher++;
 					}
 				}
+
+				// If we were constructing a begin, but r > end - begin, then we're also at end.
+				m_at_end = m_at_end || (!m_members.empty() && (m_members.back() == m_end));
 			}
 
 			bool operator==(const const_iterator &rhs) const
 			{
 				return (m_begin == rhs.m_begin)
 					&& (m_end == rhs.m_end)
-					&& (m_members == rhs.m_members);
+					&& (m_members == rhs.m_members)
+					&& (m_at_end == rhs.m_at_end);
+			}
+
+			const_iterator& operator++()
+			{
+				increment();
+				return *this;
+			}
+
+			const_iterator operator++(int)
+			{
+				const_iterator temp{*this};
+				increment();
+				return temp;
+			}
+
+			const std::set<Key, Compare, Allocator>& operator*()
+			{
+				calculate_value();
+				return m_value;
 			}
 
 		private:
+
+			void calculate_value()
+			{
+				m_value.clear();
+				for (auto &x : m_members)
+					m_value.insert(*x);
+			}
+
+			void increment()
+			{
+				// Increment if not at end
+				if (!m_members.empty() && !m_at_end)
+				{
+					auto gopher = m_members.end();
+					auto reference = m_end;
+
+					// Find the first member from the end that has room to move forward
+					while ((gopher != m_members.begin())
+						&& (*--gopher != --reference))
+					{}
+
+					// Move it forward and put the rest right after
+					for (reference = *gopher; gopher != m_members.end(); gopher++)
+						(*gopher) = ++reference;
+				}
+
+				m_at_end = m_members.empty() || (m_members.back() == m_end);
+			}
 
 			source_iterator m_begin;
 			source_iterator m_end;
 			size_type m_r;	// r as in nCr.  I might not need this, because it is embedded in m_members.
 			std::vector<source_iterator, Allocator> m_members;
+			std::set<Key, Compare, Allocator> m_value;	// The value returned by dereferencing.
+			bool m_at_end;	// If m_r == 0, then m_members is always empty and there is no distiction
+				              // between begin and end.  This flag will indicate when the end has been reached.
 		};
 
 		combinations(source_iterator source_begin, source_iterator source_end, size_type r)
@@ -134,9 +189,9 @@ namespace szwast
 
 		bool operator==(const combinations &rhs) const
 		{
-			return (m_begin == rhs.m_begin)
-				&& (m_end == rhs.m_end)
-				&& (m_r == rhs.m_r);
+			return (m_r == rhs.m_r)
+				&& (((m_end == rhs.m_end) && (m_begin == rhs.m_begin))
+					|| ((size() == rhs.size()) && std::equal(m_begin, m_end, rhs.m_begin)));
 		}
 
 		const_iterator begin() const
@@ -155,6 +210,7 @@ namespace szwast
 		}
 
 	private:
+
 		size_type evaluate_size() const
 		{
 			auto n = std::distance(m_begin, m_end);
@@ -164,6 +220,7 @@ namespace szwast
 				(size *= n) /= r;
 			return size;
 		};
+
 		source_iterator m_begin;
 		source_iterator m_end;
 		size_type m_r;	// 'r' as in nCr.
